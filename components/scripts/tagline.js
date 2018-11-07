7f47a62c7d2d013595b5bab5bb2e18fd752e431f
Paul <odriscop@eircom.net>
2018-11-07 14:37:02 +0000

    reload

--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -1,7 +1,9 @@
-var fill;
+var $, fill;
+
+$ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append(`${item}`);
-})('The most creative minds in Art');
+})('The Most Creative Minds in Art');
 
 fill;
